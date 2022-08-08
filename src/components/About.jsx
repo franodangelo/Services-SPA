0148e270a78c51167e2b80f1a17cb0fa16bbb3e1
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-08 17:12:07 -0300

    working on about and support components

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <main className='w-full my-32'>
+            <div className='max-w-[1280px] mx-auto'>
+                <section className='text-center'>
+                    <h1 className='text-5xl font-bold'>Trusted by +1000 users across the world</h1>
+                    <p className='font-sans text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, illum. Odit harum enim dolorem in nostrum ex fuga odio veritatis, omnis dicta sit, sapiente voluptatum illo qui voluptatem? Voluptas, rerum!</p>
+                </section>
+                <section className='grid md:grid-cols-3 gap-4 px-2 text-center'>
+                    <div className='border py-8 rounded-xl shadow-xl'>
+                        <p className='text-6xl font-bold text-yellow-500'>100%</p>
+                        <p className='font-sans font-semibold text-gray-400 mt-2'>Completion</p>
+                    </div>
+                    <div className='border py-8 rounded-xl shadow-xl'>
+                        <p className='text-6xl font-bold text-yellow-500'>24/7</p>
+                        <p className='font-sans font-semibold text-gray-400 mt-2'>Support</p>
+                    </div>
+                    <div className='border py-8 rounded-xl shadow-xl'>
+                        <p className='text-6xl font-bold text-yellow-500'>95%</p>
+                        <p className='font-sans font-semibold text-gray-400 mt-2'>Satisfaction Level!</p>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+};
---
+++ src/components/Support.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { PhoneIcon, ArrowSmRightIcon } from '@heroicons/react/outline';
+
+export default function Support() {
+    return (
+        <main className='w-full h-screen mt-24'>
+
+            <div className='w-full h-[700px] bg-gray-900/90 absolute'>
+                <img className='w-full h-full object-cover mix-blend-overlay' src="" alt="/" />
+            </div>
+
+            <div className='max-w-[1280px] mx-auto text-white relative'>
+                
+                <div className='px-4 py-12'>
+                    <h1 className='text-5xl font-bold py-6 text-slate-300 text-center'>Support</h1>
+                    <p className='text-2xl py-4 text-slate-300'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum quis pariatur eveniet beatae, voluptates esse inventore enim impedit ipsam soluta maiores quibusdam id repellat obcaecati sit incidunt dicta, blanditiis nemo!</p>
+                </div>
+
+                <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+                    <div className='bg-white rounded-xl shadow-2xl'>
+                        <div className='p-8'>
+                            <PhoneIcon className='w-16 p-4 bg-yellow-600 text-white rounded-lg mt-[-4rem]' />
+                            <h2 className='text-2xl font-bold my-6'>Sales</h2>
+                            <p className='font-sans text-xl text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, laboriosam?</p>
+                        </div>
+                        <div className='bg-slate-100 pl-8 py-4 rounded-b-xl'>
+                            <h3 className='flex items-center text-yellow-600'>Contact us <ArrowSmRightIcon className='w-5 mt-2' /></h3>
+                        </div>
+                    </div>
+                    
+                    <div className='bg-white rounded-xl shadow-2xl'>
+                        <div className='p-8'>
+                            <PhoneIcon className='w-16 p-4 bg-yellow-600 text-white rounded-lg mt-[-4rem]' />
+                            <h2 className='text-2xl font-bold my-6'>Sales</h2>
+                            <p className='font-sans text-xl text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, laboriosam?</p>
+                        </div>
+                        <div className='bg-slate-100 pl-8 py-4 rounded-b-xl'>
+                            <h3 className='flex items-center text-yellow-600'>Contact us <ArrowSmRightIcon className='w-5 mt-2' /></h3>
+                        </div>
+                    </div>
+
+                    <div className='bg-white rounded-xl shadow-2xl'>
+                        <div className='p-8'>
+                            <PhoneIcon className='w-16 p-4 bg-yellow-600 text-white rounded-lg mt-[-4rem]' />
+                            <h2 className='text-2xl font-bold my-6'>Sales</h2>
+                            <p className='font-sans text-xl text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, laboriosam?</p>
+                        </div>
+                        <div className='bg-slate-100 pl-8 py-4 rounded-b-xl'>
+                            <h3 className='flex items-center text-yellow-600'>Contact us <ArrowSmRightIcon className='w-5 mt-2' /></h3>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </main>
+    )
+};
