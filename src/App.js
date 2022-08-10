3027a12125c8958721e322cf7bc35b25da573627
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-10 18:55:37 -0300

    started working on benefits section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Support from './components/Support';
+import Benefits from './components/Benefits';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <About/>
       <Support/>
+      <Benefits/>
     </>
   );
 };
