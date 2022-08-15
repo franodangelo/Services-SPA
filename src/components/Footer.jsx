d26ea5acaf33cf0abfe40dae7376bfacf4332249
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-15 18:51:34 -0300

    linkedin and github linked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,58 +5,58 @@ import { FiMail } from 'react-icons/fi';
 export default function Footer() {
     return (
         <main className='w-full mt-32 bg-slate-900 text-slate-300 p-2'>
-            <section className='max-w-[1280px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
+            <section className='max-w-[1280px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-slate-600 py-8'>
                 <div>
                     <h1 className='font-sans font-bold text-md uppercase pt-2'>Solutions</h1>
-                    <ul className='justify-start'>
-                        <li className='py-1'>Marketing</li>
-                        <li className='py-1'>Analytics</li>
-                        <li className='py-1'>Data</li>
-                        <li className='py-1'>Cloud</li>
-                    </ul>
+                    <div className='justify-start'>
+                        <h2 className='py-1'>Marketing</h2>
+                        <h2 className='py-1'>Analytics</h2>
+                        <h2 className='py-1'>Data</h2>
+                        <h2 className='py-1'>Cloud</h2>
+                    </div>
                 </div>
                 <div>
                     <h1 className='font-sans font-bold text-md uppercase pt-2'>Support</h1>
-                    <ul className='justify-start'>
-                        <li className='py-1'>Pricing</li>
-                        <li className='py-1'>Documentation</li>
-                        <li className='py-1'>Guides</li>
-                        <li className='py-1'>API Status</li>
-                    </ul>
+                    <div className='justify-start'>
+                        <h2 className='py-1'>Pricing</h2>
+                        <h2 className='py-1'>Documentation</h2>
+                        <h2 className='py-1'>Guides</h2>
+                        <h2 className='py-1'>API Status</h2>
+                    </div>
                 </div>
                 <div>
                     <h1 className='font-sans font-bold text-md uppercase pt-2'>Company</h1>
-                    <ul className='justify-start'>
-                        <li className='py-1'>About</li>
-                        <li className='py-1'>Blog</li>
-                        <li className='py-1'>Jobs</li>
-                        <li className='py-1'>Clients</li>
-                    </ul>
+                    <div className='justify-start'>
+                        <h2 className='py-1'>About</h2>
+                        <h2 className='py-1'>Blog</h2>
+                        <h2 className='py-1'>Jobs</h2>
+                        <h2 className='py-1'>Ch2ents</h2>
+                    </div>
                 </div>
                 <div>
                     <h1 className='font-sans font-bold text-md uppercase pt-2'>Legal</h1>
-                    <ul className='justify-start'>
-                        <li className='py-1'>Terms</li>
-                        <li className='py-1'>Privacy</li>
-                        <li className='py-1'>Claims</li>
-                        <li className='py-1'>Conditions</li>
-                    </ul>
+                    <div className='justify-start'>
+                        <h2 className='py-1'>Terms</h2>
+                        <h2 className='py-1'>Privacy</h2>
+                        <h2 className='py-1'>Claims</h2>
+                        <h2 className='py-1'>Conditions</h2>
+                    </div>
                 </div>
                 <div className='col-span-2 pt-8 md:pt-2'>
                     <h1 className='font-bold uppercase'>Subscribe to our newsletter</h1>
                     <p className='py-4 font-sans'>The latest news, articles and resources, sent to your inbox weekly.</p>
                     <form className='flex flex-col sm:flex-row'>
-                        <input className='w-full p-2 mr-4 rounded-md mb-4 caret-yellow-400' type="email" placeholder='Enter email' />
-                        <button className='p-2 mb-4'>Subscribe</button>
+                        <input className='w-full p-2 mr-4 mb-4 rounded-md caret-amber-400' type="email" placeholder='Enter email' />
+                        <button className='mb-4'>Subscribe</button>
                     </form>
                 </div>
             </section>
-            <section className='flex flex-col max-w-[1280px] mx-auto px-2 py-4 justify-between sm:flex-row text-center text-gray-500'>
+            <section className='flex flex-col max-w-[1280px] mx-auto px-2 py-4 justify-between sm:flex-row text-center text-slate-500'>
                 <h1 className='py-4'>Franco D'Angelo - 2022. All rights reserved</h1>
                 <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
-                    <FiMail className='w-7 h-7 p-2 mx-2 bg-yellow-400 text-white rounded-2xl' />
-                    <FaLinkedin className='w-7 h-7 p-2 mx-2 bg-yellow-400 text-white rounded-2xl' />
-                    <FaGithub className='w-7 h-7 p-2 mx-2 bg-yellow-400 text-white rounded-2xl' />
+                    <FiMail className='w-7 h-7 p-2 mx-2 bg-amber-400 text-white rounded-2xl' />
+                    <a href='https://www.linkedin.com/in/franco-dangelo/'><FaLinkedin className='w-7 h-7 p-2 mx-2 bg-amber-400 text-white rounded-2xl cursor-pointer' /></a>
+                    <a href='https://github.com/franodangelo'><FaGithub className='w-7 h-7 p-2 mx-2 bg-amber-400 text-white rounded-2xl cursor-pointer' /></a>
                 </div>
             </section>
         </main>
