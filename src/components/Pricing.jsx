2205a4c80bd73e2e7d61cb5f56035d6b9613fa53
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-15 15:15:19 -0300

    started working on pricing section

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { CheckIcon, XIcon } from '@heroicons/react/outline';
+
+export default function Pricing() {
+    return (
+        <main className='w-full text-white my-24'>
+            <div className='w-full h-[800px] bg-slate-800 absolute mix-blend-overlay'>
+                <div className='max-w-[1280px] mx-auto py-12'>
+                    <div className='text-center py-8 text-slate-300'>
+                        <h3 className='font-sans font-light text-3xl uppercase'>Pricing</h3>
+                        <h1 className='text-5xl font-bold text-white py-8'>Here are our alternatives for your budget</h1>
+                        <p className='font-sans text-2xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt blanditiis, temporibus aliquid hic ex quia repellat similique voluptas aperiam sequi.</p>
+                    </div>
+                    <section className='grid md:grid-cols-3'>
+                        <div className='bg-white text-slate-800 m-2 p-8 rounded-xl shadow-2xl relative'>
+                            <span className='px-4 py-1 bg-yellow-200 text-yellow-800 rounded-2xl font-sans font-semibold text-sm uppercase'>Standard</span>
+                            <h1 className='my-6 text-4xl font-bold'>$49 p{`/`}month</h1>
+                            <p className='font-sans text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, consectetur.</p>
+                            <ul className='text-lg relative'>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><XIcon className='w-12 mr-2 text-red-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><XIcon className='w-12 mr-2 text-red-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><XIcon className='w-12 mr-2 text-red-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><XIcon className='w-12 mr-2 text-red-600' />Lorem ipsum dolor sit.</li>
+                            </ul>
+                            <button className='w-full'>Get Started</button>
+                        </div>
+                        <div className='bg-white text-slate-800 m-2 p-8 rounded-xl shadow-2xl'>
+                            <span className='px-4 py-1 bg-slate-600 rounded-2xl font-sans font-semibold text-sm text-yellow-500 uppercase'>Pro</span>
+                            <h1 className='my-6 text-4xl font-bold'>$79 p{`/`}month</h1>
+                            <p className='font-sans text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, consectetur.</p>
+                            <ul className='text-lg relative'>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><XIcon className='w-12 mr-2 text-red-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><XIcon className='w-12 mr-2 text-red-600' />Lorem ipsum dolor sit.</li>
+                            </ul>
+                            <button className='w-full'>Get Started</button>
+                        </div>
+                        <div className='bg-white text-slate-800 m-2 p-8 rounded-xl shadow-2xl'>
+                            <span className='px-4 py-1 bg-slate-100 rounded-2xl border border-slate-400 font-sans font-semibold text-sm text-slate-500 uppercase'>Enterprise</span>
+                            <h1 className='my-6 text-4xl font-bold'>$99 p{`/`}month</h1>
+                            <p className='font-sans text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, consectetur.</p>
+                            <ul className='text-lg relative'>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                                <li className='flex items-center'><CheckIcon className='w-12 mr-2 text-green-600' />Lorem ipsum dolor sit.</li>
+                            </ul>
+                            <button className='w-full'>Get Started</button>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </main>
+    )
+};
