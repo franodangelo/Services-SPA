1146a4dc2e0643fc36a5d09bb84751a57153fef0
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-15 11:10:33 -0300

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,64 @@
-import React, { useState } from "react";
-import { Link } from 'react-scroll';
+import { useState } from "react";
+import { Link } from "react-scroll";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
 
     const [nav, setNav] = useState(false);
 
-    function handleClickNav() {
+    function handleNav() {
         setNav(!nav);
-    };
-
-    function handleClose() {
-        setNav(!nav);
-    };
+    }
 
     return (
-        <div className="w-screen h-[80px] z-10 bg-white fixed drop-shadow-md">
-            <div className="flex justify-between items-center px-4 w-full h-full">
-                <div className="flex items-center">
-                    <h1 className="text-2xl font-bold mx-4">My brand.</h1>
-                    <ul className="hidden md:flex">
-                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150"><Link to="home" smooth={true} offset={-150} duration={1000}>Home</Link></li>
-                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150"><Link to="about" smooth={true} offset={-220} duration={1000}>About</Link></li>
-                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150"><Link to="support" smooth={true} offset={-40} duration={1000}>Support</Link></li>
-                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150"><Link to="benefits" smooth={true} offset={-100} duration={1000}>Benefits</Link></li>
-                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150"><Link to="pricing" smooth={true} offset={-50} duration={1000}>Pricing</Link></li>
+        <nav className="fixed w-screen h-20 z-10 bg-white shadow-md transition-all">
+            <div className="flex w-full h-full px-10 justify-between items-center">
+                <section className="flex items-center gap-6">
+                    <h1 className="font-bold text-2xl">My brand.</h1>
+                    <ul className="hidden md:flex gap-6">
+                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150">
+                            <Link to="home" smooth={true} offset={-150} duration={1000}>Home</Link>
+                        </li>
+                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150">
+                            <Link to="about" smooth={true} offset={-220} duration={1000}>About</Link>
+                        </li>
+                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150">
+                            <Link to="support" smooth={true} offset={-40} duration={1000}>Support</Link>
+                        </li>
+                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150">
+                            <Link to="benefits" smooth={true} offset={-100} duration={1000}>Benefits</Link>
+                        </li>
+                        <li className="font-sans cursor-pointer hover:text-amber-400 hover:font-bold hover:ease-in duration-150">
+                            <Link to="pricing" smooth={true} offset={-50} duration={1000}>Pricing</Link>
+                        </li>
                     </ul>
-                </div>
-                <div className="hidden md:flex pr-4 mr-4">
-                    <button>Get started</button>
-                </div>
-                <div className="md:hidden mr-4" onClick={handleClickNav}>
-                    {!nav ? <MenuIcon className="w-6 cursor-pointer" /> : <XIcon className="w-6 cursor-pointer" />}
-                </div>
+                </section>
+                <button className="hidden md:flex">Get started</button>
+                {!nav ?
+                    <MenuIcon onClick={handleNav} className="w-6 cursor-pointer md:hidden" /> :
+                    <XIcon onClick={handleNav} className="w-6 cursor-pointer md:hidden" />
+                }
             </div>
-            <ul className={!nav ? "hidden" : "absolute bg-white w-full px-8 md:hidden"}>
-                <li className="border-b-2 border-amber-200 w-full font-sans cursor-pointer"><Link onClick={handleClose} to="home" smooth={true} offset={-150} duration={1000}>Home</Link></li>
-                <li className="border-b-2 border-amber-200 w-full font-sans cursor-pointer"><Link onClick={handleClose} to="about" smooth={true} offset={-220} duration={1000}>About</Link></li>
-                <li className="border-b-2 border-amber-200 w-full font-sans cursor-pointer"><Link onClick={handleClose} to="support" smooth={true} offset={-40} duration={1000}>Support</Link></li>
-                <li className="border-b-2 border-amber-200 w-full font-sans cursor-pointer"><Link onClick={handleClose} to="benefits" smooth={true} offset={-100} duration={1000}>Benefits</Link></li>
-                <li className="border-b-2 border-amber-200 w-full font-sans cursor-pointer"><Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={1000}>Pricing</Link></li>
-                <div className="flex flex-col my-4">
-                    <button className="py-2 px-8">Get started</button>
-                </div>
-            </ul>
-        </div>
+            <div className={!nav ? "hidden" : "absolute bg-white w-full px-10 py-6 md:hidden"}>
+                <ul className="flex flex-col gap-6">
+                    <li className="pb-2 border-b-2 border-amber-200 font-sans font-medium cursor-pointer">
+                        <Link onClick={handleNav} to="home" smooth={true} offset={-150} duration={1000}>Home</Link>
+                    </li>
+                    <li className="pb-2 border-b-2 border-amber-200 font-sans font-medium cursor-pointer">
+                        <Link onClick={handleNav} to="about" smooth={true} offset={-220} duration={1000}>About</Link>
+                    </li>
+                    <li className="pb-2 border-b-2 border-amber-200 font-sans font-medium cursor-pointer">
+                        <Link onClick={handleNav} to="support" smooth={true} offset={-40} duration={1000}>Support</Link>
+                    </li>
+                    <li className="pb-2 border-b-2 border-amber-200 font-sans font-medium cursor-pointer">
+                        <Link onClick={handleNav} to="benefits" smooth={true} offset={-100} duration={1000}>Benefits</Link>
+                    </li>
+                    <li className="pb-2 border-b-2 border-amber-200 font-sans font-medium cursor-pointer">
+                        <Link onClick={handleNav} to="pricing" smooth={true} offset={-50} duration={1000}>Pricing</Link>
+                    </li>
+                </ul>
+                <button className="w-full mt-6">Get started</button>
+            </div>
+        </nav>
     )
 };
