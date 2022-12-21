082381adec9856f1a909817f15f64e0adbefb93b
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-21 16:41:23 -0300

    cleaned up file and structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,24 @@
 export default function About() {
     return (
-        <main name="about" className="w-full my-32">
-            <div className="max-w-[1280px] mx-auto text-slate-900">
-                <section className="text-center">
-                    <h1 className="mx-2 text-3xl font-bold py-6 text-center md:text-5xl">Trusted by +1000 users across the world</h1>
-                    <p className="mx-2 py-2 font-sans text-lg text-center text-slate-500 md:text-2xl md:py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, illum. Odit harum enim dolorem in nostrum ex fuga odio veritatis, omnis dicta sit, sapiente voluptatum illo qui voluptatem? Voluptas, rerum!</p>
-                </section>
-                <section className="grid md:grid-cols-3 gap-8 px-2 my-4 text-center">
-                    <div className="border py-8 rounded-xl shadow-xl">
-                        <p className="text-6xl font-bold text-amber-400">100%</p>
-                        <p className="font-sans font-semibold text-slate-400 mt-2">Completion</p>
-                    </div>
-                    <div className="border py-8 rounded-xl shadow-xl">
-                        <p className="text-6xl font-bold text-amber-400">24/7</p>
-                        <p className="font-sans font-semibold text-slate-400 mt-2">Support</p>
-                    </div>
-                    <div className="border py-8 rounded-xl shadow-xl">
-                        <p className="text-6xl font-bold text-amber-400">95%</p>
-                        <p className="font-sans font-semibold text-slate-400 mt-2">Satisfaction Level!</p>
-                    </div>
-                </section>
-            </div>
+        <main name="about" className="w'full my-32 mx-auto text-slate-900">
+            <section className="text-center">
+                <h1 className="py-6 font-bold text-3xl md:text-5xl text-center">Trusted by +1000 users across the world</h1>
+                <p className="py-2 md:py-6 font-sans text-lg md:text-2xl text-center text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, illum. Odit harum enim dolorem in nostrum ex fuga odio veritatis, omnis dicta sit, sapiente voluptatum illo qui voluptatem? Voluptas, rerum!</p>
+            </section>
+            <section className="grid md:grid-cols-3 my-4 px-2 gap-8 text-center">
+                <div className="flex flex-col py-8 gap-2 rounded-xl shadow-xl">
+                    <h3 className="font-bold text-4xl text-amber-400">100%</h3>
+                    <p className="font-sans font-semibold text-slate-400">Completion</p>
+                </div>
+                <div className="flex flex-col py-8 gap-2 rounded-xl shadow-xl">
+                    <h3 className="font-bold text-4xl text-amber-400">24/7</h3>
+                    <p className="font-sans font-semibold text-slate-400">Support</p>
+                </div>
+                <div className="flex flex-col py-8 gap-2 rounded-xl shadow-xl">
+                    <h3 className="font-bold text-4xl text-amber-400">95%</h3>
+                    <p className="font-sans font-semibold text-slate-400">Satisfaction Level!</p>
+                </div>
+            </section>
         </main>
     )
 }
