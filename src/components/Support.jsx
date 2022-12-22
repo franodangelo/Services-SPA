824dc157f3cbd8c6ef2aa34684b6147c116be9e7
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-22 07:03:32 -0300

    cleaned up file and structure

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -2,43 +2,33 @@ import { PhoneIcon, ChipIcon, FingerPrintIcon, ArrowSmRightIcon } from "@heroico
 
 export default function Support() {
     return (
-        <main name="support" className="w-full my-32 bg-slate-800">
-            <div className="max-w-[1280px] mx-auto text-white relative">
-                <div className="px-4 py-12">
-                    <h1 className="text-3xl font-bold py-6 text-center md:text-5xl">Need support?</h1>
-                    <p className="py-2 font-sans text-lg text-center md:text-2xl md:py-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum quis pariatur eveniet beatae, voluptates esse inventore enim impedit ipsam soluta maiores quibusdam id repellat obcaecati sit incidunt dicta, blanditiis nemo!</p>
+        <main name="support" className="w-full py-16 sm:py-20 md:py-24 lg:py-32 px-10 md:px-16 lg:px-20 bg-slate-800 text-white">
+            <h1 className="py-2 md:py-4 lg:py-6 font-bold text-3xl md:text-5xl text-center">Need support?</h1>
+            <p className="py-2 md:py-4 font-sans text-lg md:text-2xl md:text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum quis pariatur eveniet beatae, voluptates esse inventore enim impedit ipsam soluta maiores quibusdam id repellat obcaecati sit incidunt dicta, blanditiis nemo!</p>
+            <div className="grid grid-cols-1 lg:grid-cols-3 mt-16 gap-16 lg:gap-6">
+                <div className="bg-white rounded-xl shadow-2xl">
+                    <div className="p-8">
+                        <ChipIcon className="w-16 -mt-16 p-4 text-white rounded-lg bg-amber-400" />
+                        <h2 className="my-6 font-bold text-2xl text-slate-900">Online data</h2>
+                        <p className="font-sans text-xl text-slate-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ea deleniti magni.</p>
+                    </div>
+                    <h3 className="flex pl-8 py-4 items-center text-lg text-amber-400 rounded-b-xl bg-slate-100">Download now <ArrowSmRightIcon className="w-5 ml-2" /></h3>
                 </div>
-                <div className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 py-8 sm:pt-20">
-                    <div className="bg-white rounded-xl shadow-2xl">
-                        <div className="p-8">
-                            <ChipIcon className="w-16 p-4 bg-amber-400 text-white rounded-lg mt-[-4rem]" />
-                            <h2 className="my-6 font-bold text-2xl text-slate-900">Online data</h2>
-                            <p className="font-sans text-xl text-slate-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ea deleniti magni.</p>
-                        </div>
-                        <div className="bg-slate-100 pl-8 py-4 rounded-b-xl">
-                            <h3 className="flex items-center text-amber-400 text-lg">Download now <ArrowSmRightIcon className="w-5 mt-2" /></h3>
-                        </div>
+                <div className="bg-white rounded-xl shadow-2xl">
+                    <div className="p-8">
+                        <PhoneIcon className="w-16 -mt-16 p-4 text-white rounded-lg bg-amber-400" />
+                        <h2 className="my-6 font-bold text-2xl text-slate-900">Sales</h2>
+                        <p className="font-sans text-xl text-slate-600">Et voluptas ipsa id facere veritatis exercitationem similique inventore nam, fuga porro cum laborum!</p>
                     </div>
-                    <div className="bg-white rounded-xl shadow-2xl">
-                        <div className="p-8">
-                            <PhoneIcon className="w-16 p-4 bg-amber-400 text-white rounded-lg mt-[-4rem]" />
-                            <h2 className="my-6 font-bold text-2xl text-slate-900">Sales</h2>
-                            <p className="font-sans text-xl text-slate-600">Et voluptas ipsa id facere veritatis exercitationem similique inventore nam, fuga porro cum laborum!</p>
-                        </div>
-                        <div className="bg-slate-100 pl-8 py-4 rounded-b-xl">
-                            <h3 className="flex items-center text-amber-400 text-lg">Contact us <ArrowSmRightIcon className="w-5 mt-2" /></h3>
-                        </div>
+                    <h3 className="flex pl-8 py-4 items-center text-lg text-amber-400 rounded-b-xl bg-slate-100">Contact us <ArrowSmRightIcon className="w-5 ml-2" /></h3>
+                </div>
+                <div className="bg-white rounded-xl shadow-2xl">
+                    <div className="p-8">
+                        <FingerPrintIcon className="w-16 -mt-16 p-4 text-white rounded-lg bg-amber-400" />
+                        <h2 className="my-6 font-bold text-2xl text-slate-900">Security</h2>
+                        <p className="font-sans text-xl text-slate-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, laboriosam?</p>
                     </div>
-                    <div className="bg-white rounded-xl shadow-2xl">
-                        <div className="p-8">
-                            <FingerPrintIcon className="w-16 p-4 bg-amber-400 text-white rounded-lg mt-[-4rem]" />
-                            <h2 className="my-6 font-bold text-2xl text-slate-900">Security</h2>
-                            <p className="font-sans text-xl text-slate-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, laboriosam?</p>
-                        </div>
-                        <div className="bg-slate-100 pl-8 py-4 rounded-b-xl">
-                            <h3 className="flex items-center text-amber-400 text-lg">Run a test <ArrowSmRightIcon className="w-5 mt-2" /></h3>
-                        </div>
-                    </div>
+                    <h3 className="flex pl-8 py-4 items-center text-lg text-amber-400 rounded-b-xl bg-slate-100">Run a test <ArrowSmRightIcon className="w-5 ml-2" /></h3>
                 </div>
             </div>
         </main>
