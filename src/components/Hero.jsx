e6bddc389a5878a804066b0fa913ff9121e58054
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-22 15:20:25 -0300

    more margin for the works element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import bgImg from "../assets/landing-bg.svg";
 
 export default function Hero() {
     return (
-        <header name="home" className="flex flex-col w-full py-[120px] md:py-40 justify-between items-center bg-zinc-100 text-slate-900">
+        <header name="home" className="flex flex-col w-full pt-[120px] md:pt-40 justify-between items-center bg-zinc-100 text-slate-900">
             <section className="flex flex-col md:flex-row w-full px-10 md:px-16 lg:px-20 md:items-center justify-between gap-4 md:gap-0">
                 <div className="flex flex-col basis-2/3 gap-2">
                     <h4 className="font-sans font-light text-md md:text-base">Eveniet non ab laudantium, temporibus animi.</h4>
@@ -16,7 +16,7 @@ export default function Hero() {
                 </div>
                 <img className="basis-1/3 w-full h-full mt-4 md:mt-0 object-cover" src={bgImg} alt="Services illustration" />
             </section>
-            <section className="flex flex-col w-[80%] sm:w-[75%] lg:w-[65%] xl:w-[50%] mt-16 p-4 gap-4 text-center rounded-xl bg-white shadow-lg">
+            <section className="flex flex-col w-[80%] sm:w-[75%] lg:w-[65%] xl:w-[50%] my-20 p-4 gap-4 text-center rounded-xl bg-white shadow-lg">
                 <h3 className="font-sans text-2xl">Our works</h3>
                 <div className="grid grid-cols-2 sm:grid-cols-4 justify-around">
                     <h4 className="flex px-4 py-2 gap-2 text-slate-600">
